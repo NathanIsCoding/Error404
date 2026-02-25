0829ed86637ecfa1640c9bf431f0ddce358d4402
gbrown99 <144386528+gbrown99@users.noreply.github.com>
2026-02-25 09:18:26 -0800

    Quick fix
    Minor formatting fix.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,41 +1,41 @@
 export default function Navbar() {
-  return (
-    <header style={styles.bar}>
-      <div style={styles.brand}>ERROR 404 Job Not Found:</div>
+   return (
+       <header style={styles.bar}>
+       <div style={styles.brand}>ERROR 404 Job Not Found:</div>
 
-      <nav style={styles.navLink}>
-        <a href="#signin" style={styles.link}>Sign In</a>
-        <a href="#createAccount" style={styles.link}>Create Account</a>
-        <a href="#jobs" style={styles.link}>Browse Job Opportunites</a>
-      </nav>
-    </header>
-  );
-}
+        <nav style={styles.navLink}>
+            <a href="#signin" style={styles.link}>Sign In</a>
+            <a href="#createAccount" style={styles.link}>Create Account</a>
+            <a href="#jobs" style={styles.link}>Browse Job Opportunites</a>
+        </nav>
+        </header>
+    );
+    }
 
-const styles = {
-  bar: {
-    backgroundColor: "#135379",
-    color: "white",
+    const styles = {
+    bar: {
+        backgroundColor: "#135379",
+        color: "white",
 
-    display: "flex",
-    justifyContent: "space-between",
-    padding: "12px 22px",
-    width: "100%",
-  },
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "12px 22px",
+        width: "100%",
+    },
 
-  brand: {
-    fontWeight: "bold",
-    color: "white",
-    fontSize: 20,
-  },
+    brand: {
+        fontWeight: "bold",
+        color: "white",
+        fontSize: 20,
+    },
 
-  navLink: {
-    display: "flex",
-    gap: 90,
-  },
+    navLink: {
+        display: "flex",
+        gap: 90,
+    },
 
-  link: {
-    textDecoration: "none",
-    color:"#bfb8b9",
-  },
+    link: {
+        textDecoration: "none",
+        color:"#bfb8b9",
+    },
 };
