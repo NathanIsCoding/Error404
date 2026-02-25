736ed263652d08d69bc61c02526591b4aed6418e
Nathan <nworth013@gmail.com>
2026-02-25 09:38:28 -0800

    Added a basic Job Card component, and stylesheet. Will need to re-organize structure of components.

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,23 @@
+import './JobCard.css';
+import reactLogo from '../assets/react.svg'
+
+export function JobCard() { 
+
+    function clicked(){ 
+        alert('CLICKED!');
+    }
+    
+    return(
+        <div className='card'>
+            <img src={reactLogo} className='cardImg'></img>
+            <div className='textRow'>
+                <p className='text'>Company Title - Position Name</p>
+                <p className='text'>Salary/Hourly Wage</p>
+            </div>
+            <button onClick={clicked} className='expandButton'>Expand</button>
+            <button onClick={clicked}>Apply</button>
+        </div>
+    );
+
+}
+
