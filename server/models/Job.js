ce735306f6705ef71029acf8f04b10965cf113e5
Turbileg Uurtsaikh <turbileg@Mac.lan>
2026-03-18 12:06:03 -0700

    Add chema models (User, Job, Resume, CoverLetter)

---
+++ server/models/Job.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  jobId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  jobType: {
+    type: String,
+    enum: ['full-time', 'part-time', 'contract', 'internship'],
+    required: true
+  },
+  industry: [{
+    type: String,
+    required: true
+  }],
+  salary: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model('Job', jobSchema);
