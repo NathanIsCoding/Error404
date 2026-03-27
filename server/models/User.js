4d24993166ed927123b2dea067f6110cbc4bdae7
gbrown99 <144386528+gbrown99@users.noreply.github.com>
2026-03-27 12:35:58 -0700

    Adding isDisabled boolean to user model in database

--- server/models/User.js
+++ server/models/User.js
@@ -51,6 +51,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  isDisabled: {
+    type: Boolean,
+    default: false
+  },
   createdAt: {
     type: Date,
     default: function () {
