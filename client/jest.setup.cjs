9221467891c0a5cd2d6d4684d94fca4a677d024d
NathanIsCoding <nworth013@gmail.com>
2026-04-05 19:53:48 -0700

    cleaned up terminal output, all tests are passing

--- client/jest.setup.cjs
+++ client/jest.setup.cjs
@@ -1,3 +1,5 @@
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
